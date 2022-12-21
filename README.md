1438c3fa976fee0ca18384c07f8f80ccb07eae7b
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-12-21 11:49:13 -0300

    [NEW] O bot esta funcionando, falta fazer o deploy

---
+++ README.md
@@ -0,0 +1,2 @@
+Twitter Bot created to count and warn it's followers, everyday, how many days until the next World CUp starts.
+
---
+++ main.py
@@ -0,0 +1,42 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+import schedule
+import time
+import datetime
+
+# load environment variables
+load_dotenv()
+
+# put them in local variables
+API_KEY = os.environ.get("API_KEY")
+API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+
+def tweet_missing_days():
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # create API with authentication
+    api = tweepy.API(auth)
+
+    missing_days: int = (datetime.datetime(2026, 6, 8) - datetime.datetime.now()).days
+
+    tweet: str = f"Faltam {missing_days} para a Copa do Mundo de 2026!"
+
+    api.update_status(tweet)
+
+def main():
+    
+    schedule.every().day.at("11:47:00").do(tweet_missing_days)
+
+    while True:
+        schedule.run_pending()
+    return
+
+if __name__ == "__main__":
+    main()
